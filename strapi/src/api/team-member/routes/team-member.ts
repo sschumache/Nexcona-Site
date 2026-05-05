713a665b7dd3c953b94e7d1eeee7730e3043d5ea
Sven Schumacher <mail@svenschumacher.ch>
2026-05-05 10:41:14 +0200

    fix: modul

--- strapi/src/api/team-member/routes/team-member.ts
+++ strapi/src/api/team-member/routes/team-member.ts
@@ -1,3 +1,4 @@
+// src/api/team-member/routes/team-member.ts
 import { factories } from '@strapi/strapi';
 
 export default factories.createCoreRouter('api::team-member.team-member');
--- strapi/src/api/team-member/services/team-member.ts
+++ strapi/src/api/team-member/services/team-member.ts
@@ -1,3 +1,4 @@
+// src/api/team-member/services/team-member.ts
 import { factories } from '@strapi/strapi';
 
 export default factories.createCoreService('api::team-member.team-member');
