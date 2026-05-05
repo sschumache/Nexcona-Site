ec85c1628c3dca81fa842235b0e8853d3616f1e9
Sven Schumacher <mail@svenschumacher.ch>
2026-05-05 16:45:16 +0200

    fix: page relations

--- next/app/[locale]/(marketing)/[slug]/page.tsx
+++ next/app/[locale]/(marketing)/[slug]/page.tsx
@@ -77,7 +77,7 @@ export default async function Page({ params }: LocaleSlugParamsProps) {
   return (
     <>
       <ClientSlugHandler localizedSlugs={{ [locale]: slug }} />
-      <PageContent pageData={pageData} />
+      <PageContent pageData={{ ...pageData, locale }} />
     </>
   );
 }
